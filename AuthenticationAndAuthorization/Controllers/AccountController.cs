8bc16f5b89dc06737ab60cf5ea20560e5effe0c1
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-06-27 12:21:11 +0300

    account conteller yazıldı

---
+++ AuthenticationAndAuthorization/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using AuthenticationAndAuthorization.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationAndAuthorization.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        //AspNetUsers Tablosunun manager sınıfıdır 
+        //kısaca user ları yoneten sınıfıdır
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly IPasswordHasher<AppUser> passwordHasher;
+
+        public AccountController(UserManager<AppUser> userManager,
+                                 SignInManager<AppUser> signInManager,
+                                 IPasswordHasher<AppUser> passwordHasher)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.passwordHasher = passwordHasher;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
