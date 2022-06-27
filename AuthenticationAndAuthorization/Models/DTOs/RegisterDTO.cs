a183da45ecd17be510df53786cb9f338be48763a
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-06-27 12:40:00 +0300

    register dto yazıldı

---
+++ AuthenticationAndAuthorization/Models/DTOs/RegisterDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationAndAuthorization.Models.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "UserName Alani zorunludur.")]
+        [Display(Name = "User Name")]
+        [MinLength(2, ErrorMessage = "UserName alani en az 2 karakter olmalıdır.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email Alani zorunludur.")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "email adresini yanlış girdiniz")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Sifre Alani zorunludur.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "TcKimlik No")]
+        public string TcNo { get; set; }
+    }
+}
