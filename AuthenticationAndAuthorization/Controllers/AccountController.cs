bbf465c8b25da819f813b638636fd7f9f813688f
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-06-27 13:59:11 +0300

    logout

--- AuthenticationAndAuthorization/Controllers/AccountController.cs
+++ AuthenticationAndAuthorization/Controllers/AccountController.cs
@@ -9,15 +9,14 @@ namespace AuthenticationAndAuthorization.Controllers
     [Authorize]
     public class AccountController : Controller
     {
-        //AspNetUsers Tablosunun manager sınıfıdır 
-        //kısaca user ları yoneten sınıfıdır
+
+        // AspNetUsers Tablosunun manager sinifidir. 
+        // Kisaca user'lari yöneten siniftir.
         private readonly UserManager<AppUser> userManager;
         private readonly SignInManager<AppUser> signInManager;
         private readonly IPasswordHasher<AppUser> passwordHasher;
 
-        public AccountController(UserManager<AppUser> userManager,
-                                 SignInManager<AppUser> signInManager,
-                                 IPasswordHasher<AppUser> passwordHasher)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IPasswordHasher<AppUser> passwordHasher)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
@@ -31,12 +30,15 @@ namespace AuthenticationAndAuthorization.Controllers
             return View(registerDTO);
         }
 
+
+
         [HttpPost, ValidateAntiForgeryToken, AllowAnonymous]
         public async Task<IActionResult> Register(RegisterDTO registerDTO)
         {
             if (ModelState.IsValid)
             {
                 AppUser appUser = new AppUser { UserName = registerDTO.UserName, Email = registerDTO.Email };
+
                 var result = await userManager.CreateAsync(appUser, registerDTO.Password);
 
                 if (result.Succeeded)
@@ -52,15 +54,45 @@ namespace AuthenticationAndAuthorization.Controllers
                 }
             }
 
-
             return View(registerDTO);
-
         }
+
         [AllowAnonymous]
         public IActionResult Login(string url)
         {
 
             return View(new LoginDTO { ReturnUrl = url });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken, AllowAnonymous]
+        public async Task<IActionResult> Login(LoginDTO loginDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByNameAsync(loginDTO.UserName);
+                if (user != null)
+                {
+                    var signInResult = await signInManager.PasswordSignInAsync(user, loginDTO.Password, false, false);
+
+                    if (signInResult.Succeeded)
+                    {
+                        return RedirectToAction(loginDTO.ReturnUrl);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Kullanici Adi yada Şifre Yanliş");
+                    }
+
+                }
+
+            }
+
+            return View(loginDTO);
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Login");
         }
     }
 }
