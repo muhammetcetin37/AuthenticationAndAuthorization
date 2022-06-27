061d0ff6d693bf5439792feb8802763d3c87d8da
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-06-27 11:24:17 +0300

    Appuser içerisinde CreateDate ve Status Alanları Encapulation yapildı

---
+++ AuthenticationAndAuthorization/Infrastructure/Contexts/SqlContext.cs
@@ -0,0 +1,19 @@
+﻿using AuthenticationAndAuthorization.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationAndAuthorization.Infrastructure.Contexts
+{
+    public class SqlContext : IdentityDbContext<AppUser>
+    {
+        public SqlContext()
+        {
+
+        }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+
+        }
+    }
+}
